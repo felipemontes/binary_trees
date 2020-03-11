80ba12c6fd10e82bef676bba94d0a51ea0940fc3
Felipe <felipe_londono1@hotmail.com>
2020-03-11 22:02:17 +0000

    [FIX]: corrected conditionals

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,9 +6,8 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || !node->parent->parent || !node->parent)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
-
 	if (node->parent == node->parent->parent->left)
 		return (node->parent->parent->right);
 	return (node->parent->parent->left);
